d44b520c4da3a5973f41bf48e24813cdc9f47a16
Rahul Menon <menonrahul02@gmail.com>
2019-03-19 23:22:42 -0500

    text data

---
+++ text_net.py
@@ -0,0 +1,12 @@
+from tensorflow.keras.layers import Activation, Dense, Dropout
+from tensorflow.keras.preprocessing.text import Tokenizer
+from sklearn.preprocessing import LabelBinarizer
+from tensorflow.keras.models import Sequential
+from matplotlib import pyplot as plt
+import sklearn.datasets as skds
+from pathlib import Path
+import pandas as pd
+import numpy as np
+import pickle
+
+
